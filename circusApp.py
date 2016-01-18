77f3e4a435e85e24d00f3eb7eece6b79a159a416
Juan Restrepo <juan.restrepop@gmail.com>
2016-01-18 21:14:25 +0100

    Adds registration of new students.

--- circusApp.py
+++ circusApp.py
@@ -4,6 +4,7 @@ __author__ = 'Loops'
 import os
 import webapp2
 import jinja2
+import re
 
 from google.appengine.ext import db
 
@@ -22,7 +23,6 @@ class CircusAppHandler(webapp2.RequestHandler):
         self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
-        params['user'] = self.user
         return render_str(template, **params)
 
     def render(self, template, **kw):
@@ -39,16 +39,80 @@ class NewCourse(CircusAppHandler):
         self.render('nouveau_stage.html')
 
 ## New student page ##
+# Regular expression to check inputs #
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+TEL_RE = re.compile(r"^[0-9_-]{10}$")
+def valid_telephone(telephone):
+    return not telephone or TEL_RE.match(telephone)
+
 
 class NewStudent(CircusAppHandler):
     def get(self):
         self.render('nouveau_eleve.html')
 
+    def post(self):
+        have_error = False
+        self.nom = self.request.get('nom')
+        self.prenom = self.request.get('prenom')
+        self.age = self.request.get('age')
+        self.sexe = self.request.get('sexe')
+        self.email = self.request.get('email')
+        self.telephone = self.request.get('telephone')
+        self.location = self.request.get('location')
 
+        params = dict(nom = self.nom,
+                      prenom = self.prenom,
+                      age = self.age,
+                      email=self.email,
+                      telephone=self.telephone)
+
+        if not self.nom :
+            params['error_nom'] = "Un nom siouple"
+            have_error = True
+
+        if not self.prenom:
+            params['error_prenom'] = "Un prenom siouple"
+            have_error = True
+
+        if not self.age:
+            params['error_age'] = "L' age siouple"
+            have_error = True
+
+        if not valid_email(self.email):
+            params['error_email'] = "Email invalide"
+            have_error = True
+
+        if not valid_telephone(self.telephone):
+            params['error_telephone'] = "Telephone invalable"
+            have_error = True
+
+        if have_error:
+            self.render('nouveau_eleve.html', **params)
+        else:
+            self.done()
+
+    def done(self):
+        raise NotImplementedError
+
+class NewStudentRegistration(NewStudent):
+    def done(self):
+        self.redirect('ajouter-eleve/bienvenue?nom=' + self.nom + '&prenom=' + self.prenom)
+
+
+class Bienvenue(CircusAppHandler):
+    def get(self):
+        nom = self.request.get('nom')
+        prenom = self.request.get('prenom')
+
+        self.render('bienvenue.html', nom = nom, prenom = prenom)
 
 ### And the handler of everything
 app = webapp2.WSGIApplication([('/', MainPage),
                                 ('/ajouter-stage', NewCourse),
-                                ('/ajouter-eleve', NewStudent)
+                                ('/ajouter-eleve', NewStudentRegistration),
+                                ('ajouter-eleve/bienvenue', Bienvenue)
                              ],
                               debug=True)
