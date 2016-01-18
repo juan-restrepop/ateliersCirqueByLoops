3942f45e1672edd9b5bab018b18e4c2927e65feb
Juan Restrepo <juan.restrepop@gmail.com>
2016-01-18 14:51:23 +0100

    Clean up. Add rendering functions to `CircusAppHandler` class

--- circusApp.py
+++ circusApp.py
@@ -11,16 +11,23 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
-## Basic template rendering
+## Basic template rendering ##
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+## The Basic web page handler class ##
 class CircusAppHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-## The main page
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+## The main page ##
 class MainPage(CircusAppHandler):
     def get(self):
         self.write('Bonjour les circassiens!')
